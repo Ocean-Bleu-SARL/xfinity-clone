da592a729c86840ebc3a917f4055584e5c24efc7
Brice Kamhoua <kamhoua.k.brice@gmail.com>
2024-02-19 09:35:33 +0100

    added metadata

---
+++ src/config/site.metadata.ts
@@ -0,0 +1,17 @@
+const siteMetadata = {
+    title: 'Bantubeat',
+    description: 'A blog created with Next.js and Tailwind.css',
+    language: 'fr',
+    author: 'PARAZIT MUSIC',
+    siteUrl: 'https://bantubeat-front.igkdev.com',
+    siteLogo: '/logo.png',
+    socialBanner: '/logo.png',
+    email: 'address@yoursite.com',
+    github: 'https://github.com',
+    twitter: 'https://twitter.com/Twitter',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com',
+    linkedin: 'https://www.linkedin.com',
+} as const;
+
+export default siteMetadata;
